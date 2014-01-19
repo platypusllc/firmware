6918c4b0b52cb1c124acffe81fe0acf4809e8cb1
Pras Velagapudi <psigen@gmail.com>
2014-01-18 19:58:56 -0500

    Added new classes that implement connection and disconnection from USB accessory.

---
+++ software/src/main/java/com/platypus/android/server/ServerLauncherActivity.java
@@ -0,0 +1,43 @@
+package com.platypus.android.server;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Launcher that starts Platypus Server whenever accessory is plugged in.
+ * 
+ * Adapted from:
+ * https://github.com/follower/android-background-service-usb-accessory
+ * 
+ * @author pkv
+ */
+public class ServerLauncherActivity extends Activity {
+    private String TAG = "AirboatLauncherActivity";
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        Log.d(TAG, "Starting vehicle service launcher.");
+        Toast.makeText(this, "Platypus Control Board detected.", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, ServerService.class);
+        intent.fillIn(getIntent(), 0);
+        startService(intent);
+                
+        // See:
+        //
+        //    <http://permalink.gmane.org/gmane.comp.handhelds.android.devel/154481> &
+        //    <http://stackoverflow.com/questions/5567312/android-how-to-execute-main-fucntionality-of-project-only-by-clicking-on-the-ic/5567514#5567514>
+        //
+        // for combination of `Theme.NoDisplay` and `finish()` in `onCreate()`/`onResume()`.
+        //
+        finish();
+                
+        Log.d(TAG, "Exiting vehicle service launcher.");
+    }
+}
---
+++ software/src/main/java/com/platypus/android/server/ServerService.java
@@ -0,0 +1,163 @@
+/**
+ * TODO: Insert license information here
+ */
+package com.platypus.android.server;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.app.IntentService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbManager;
+import android.os.ParcelFileDescriptor;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+/**
+ * Implements a Platypus Vehicle Server. 
+ * 
+ * @author pkv
+ *
+ */
+public class ServerService extends IntentService {
+	private static final String TAG = ServerService.class.getName(); 
+
+	private UsbManager usbManager_;
+	private UsbAccessory usbAccessory_;
+	private ParcelFileDescriptor usbDescriptor_;
+	
+	private SharedPreferences prefs_; 
+	
+	public ServerService() {
+		super("Platypus Server");
+	}
+	
+	/**
+	 * Called by the system when the service is first created.
+	 */
+	@Override
+	public void onCreate() {
+		
+		// Get USB Manager to handle USB accessories.
+		usbManager_ = (UsbManager)getSystemService(Context.USB_SERVICE);
+		
+        // Listen for shared preference changes
+        SharedPreferences prefs_ = PreferenceManager.getDefaultSharedPreferences(this);
+		prefs_.registerOnSharedPreferenceChangeListener(prefListener_);
+		// TODO: set initial values for these system settings
+		
+		// Create an intent filter to listen for device disconnections
+        IntentFilter filter = new IntentFilter(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+        registerReceiver(usbReceiver_, filter);
+	}
+
+	/**
+	 * Listens for changes in the server preferences, and immediately applies them to the server.  
+	 */
+	OnSharedPreferenceChangeListener prefListener_ = new OnSharedPreferenceChangeListener() {
+		@Override
+	    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+	    	// TODO: When android supports java 1.7 compliance, change this to 'switch'.
+	    	
+	    	// Check for the key that changed and make the appropriate change to the server
+	    	if(key.equalsIgnoreCase("REGISTRY_IP")) {
+		        // TODO: Update based on new preferences
+	    	} else if(key.equalsIgnoreCase("VEHICLE_TYPE")) {
+		        // TODO: Update based on new preferences
+	    	} else if(key.equalsIgnoreCase("FAILSAFE_IP")) {
+		        // TODO: Update based on new preferences
+	    	} else {
+	    		Log.d(TAG, "Unknown preference '" + key + "' changed.");
+	    	}
+	    }
+	};
+	
+	
+	/* (non-Javadoc)
+	 * @see android.app.IntentService#onHandleIntent(android.content.Intent)
+	 */
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		
+		// Assume that we can only be launched by the ServerLauncherActivity
+		// which provides a handle to the accessory.
+		
+		// TODO: Get the accessory
+		
+		// Connect to control board.
+		usbAccessory_ = (UsbAccessory)intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+		usbDescriptor_ = usbManager_.openAccessory(usbAccessory_);
+		if (usbDescriptor_ == null) {
+			// If the accessory fails to connect, terminate service.
+			Log.e(TAG, "Failed to open accessory.");
+			return;
+		}
+
+		// Get input and output streams
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(usbDescriptor_.getFileDescriptor())));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(usbDescriptor_.getFileDescriptor())));
+		
+		// Start a loop to receive data from accessory.
+		try {
+			while(true) {
+				String response = reader.readLine();
+				// TODO: handle this response
+				//handleCommand(response);
+			}
+		} catch (IOException e) {
+			
+		}
+
+		// TODO: is this actually necessary?  The only way to get here is to close the descriptor
+		// Shutdown the connection to the accessory.
+		try {
+			usbDescriptor_.close();
+		} catch (IOException e) {
+			Log.w(TAG, "Accessory closed uncleanly.", e);
+		}
+	}
+
+	/**
+	 * Called by the system when the service is first created.
+	 */
+	@Override
+	public void onDestroy() {
+		
+		// Disconnect intent filter to listen for device disconnections
+        unregisterReceiver(usbReceiver_);
+		
+		// Unregister the shared preferences
+		prefs_.unregisterOnSharedPreferenceChangeListener(prefListener_);
+	}
+	
+	/**
+	 * Listen for disconnection events for accessory and close connection.
+	 */
+	BroadcastReceiver usbReceiver_ = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			
+			// Check if this accessory matches the one we have open.
+            UsbAccessory accessory = (UsbAccessory)intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+            if (usbAccessory_.equals(accessory)) {
+            	try {
+            		// Close the descriptor for our accessory (triggering shutdown)
+            		usbDescriptor_.close();
+            	} catch (IOException e) {
+            		Log.w(TAG, "Failed to close accessory cleanly.", e);
+            	}
+            }
+		}
+	};
+}
