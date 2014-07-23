8d03f6138a4798f7db9aee7deca9f70ef3a18f31
Pras Velagapudi <psigen@gmail.com>
2014-07-23 00:08:47 -0400

    Removed software folder (now in Server repository)
    The new location of the server is:
    https://github.com/PlatypusLLC/Server

--- software/proto/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Compiles the Platypus protocol buffer messages and places them into
-# the Android source tree.
-#
-java -jar wire-compiler-1.2.0.jar \
-    --proto_path=. \
-    --java_out=../src/main/java ./platypus.proto
